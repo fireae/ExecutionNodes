7348961154bc94ee2d975dbc75870129abb3a684
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-01 17:00:29 +0200

    add logging

---
+++ include/execution_nodes/logging.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <strstream>
+#include <utility>
+#include <vector>
+
+namespace execution_nodes {
+
+struct Log {
+
+  template <class... A> static void error(A &&...a) {
+    errorImpl(
+        {[&a](std::strstream &ststr) { ststr << std::forward<A>(a); }...});
+  }
+
+  template <class... A> static void warning(A &&...a) {
+  
+      warningImpl(
+        {[&a](std::strstream &ststr) { ststr << std::forward<A>(a); }...});
+
+  }
+
+  template <class... A> static void throwError(A &&...a) {
+    throwErrorImpl(
+        {[&a](std::strstream &ststr) { ststr << std::forward<A>(a); }...});
+  }
+
+private:
+  static void errorImpl(std::vector<std::function<void(std::strstream &)>> v);
+  static void
+  throwErrorImpl(std::vector<std::function<void(std::strstream &)>> v);
+  static void warningImpl(std::vector<std::function<void(std::strstream &)>> v);
+  struct Impl;
+  static std::unique_ptr<Impl> impl_;
+};
+} // namespace execution_nodes
---
+++ src/logging.cpp
@@ -0,0 +1,37 @@
+// log.cpp
+#include <execution_nodes/logging.hpp>
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <strstream>
+
+namespace execution_nodes {
+
+struct Log::Impl {};
+
+std::string convert(std::vector<std::function<void(std::strstream &)>> v) {
+  std::strstream sstr;
+  for (auto &&i : v) {
+    i(sstr);
+  }
+  return sstr.str();
+}
+
+void Log::errorImpl(std::vector<std::function<void(std::strstream &)>> v) {
+  spdlog::error(convert(v));
+}
+
+void Log::warningImpl(std::vector<std::function<void(std::strstream &)>> v) {
+  spdlog::warn(convert(v));
+}
+
+void Log::throwErrorImpl(std::vector<std::function<void(std::strstream &)>> v) {
+
+  auto msg = convert(v);
+  spdlog::error(msg);
+  throw std::runtime_error(msg);
+}
+
+std::unique_ptr<Log::Impl> Log::impl_ =
+    std::make_unique<Log::Impl>(Log::Impl());
+
+} // namespace execution_nodes
