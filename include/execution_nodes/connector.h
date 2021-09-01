ef4784cceddaf46c0ec6aad3d510c59e1ff5f5f8
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-01 17:49:00 +0200

    format document and add typedef

--- include/execution_nodes/connector.h
+++ include/execution_nodes/connector.h
@@ -5,10 +5,11 @@
 
 #include <execution_nodes/connection.h>
 #include <string>
+#include <memory>
 
 namespace execution_nodes {
 
-  typedef std::string PortId;
+typedef std::string PortId;
 
 class Connector {
 public:
@@ -22,19 +23,19 @@ public:
   void setObject(const PortId &portId, const std::any &&obj);
   bool hasObject(const PortId &portId);
   void getObject(const PortId &portId, std::any &obj);
-  //const std::any &getObject(ConnectionId connection);
 
   static ConnectionId hash(const std::string &str);
 
-  static PortId createPortId(const std::string &nodeName, const std::string &portName);
-
+  static PortId createPortId(const std::string &nodeName,
+                             const std::string &portName);
 
 private:
-
-  ConnectionId getConnectionByPortId(const PortId& portId);
+  ConnectionId getConnectionByPortId(const PortId &portId);
 
   std::map<ConnectionId, std::any> objectsMap_;
   std::map<PortId, ConnectionId> connectionMap_;
 };
+
+typedef std::shared_ptr<Connector> ConnectorPtr;
 
 } // namespace execution_nodes
