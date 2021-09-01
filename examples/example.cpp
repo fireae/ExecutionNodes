be54acefa2d78ef6083836902d3364c1cf3a6a96
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-01 20:53:53 +0200

    add first working example

--- examples/example.cpp
+++ examples/example.cpp
@@ -9,8 +9,10 @@ class RandomNumberGenerator : public execution_nodes::Node {
 
 public:
   RandomNumberGenerator(const NodeDefinition &definition,
-      const ConnectorPtr &connector)
-      : execution_nodes::Node(definition, connector) {
+                        const ConnectorPtr &connector)
+      : execution_nodes::Node(definition, connector),
+        distr_(std::uniform_int_distribution<int>(0, 100))
+  {
 
     uint64_t seed = getSetting<uint64_t>("seed");
     randomEngine_ = std::mt19937_64(seed);
@@ -21,6 +23,7 @@ public:
 
     int number = distr_(randomEngine_);
     setOutput("number", number);
+    std::cout << "rng says: " << number << std::endl;
   }
 
 private:
@@ -28,10 +31,61 @@ private:
   std::uniform_int_distribution<int> distr_;
 };
 
+class Adder : public execution_nodes::Node {
+
+public:
+  Adder(const NodeDefinition &definition, const ConnectorPtr &connector)
+      : execution_nodes::Node(definition, connector) {}
+
+  void execute() override {
+
+    int a = 0, b = 0;
+    getInput("a", a);
+    getInput("b", b);
+
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+
+    int sum = a + b;
+
+    setOutput("sum", sum);
+    std::cout << "sum: " << sum << std::endl;
+  }
+};
+
+class NumberPrinter : public execution_nodes::Node {
+
+public:
+  NumberPrinter(const NodeDefinition &definition, const ConnectorPtr &connector)
+      : execution_nodes::Node(definition, connector) {}
+
+  void execute() override {
+    int number = 0;
+    getInput("number", number);
+    std::cout << "The number is: " << number << std::endl;
+  }
+};
+
 int main() {
 
-    ConnectorPtr connector = std::make_shared<Connector>();
+  ConnectorPtr connector = std::make_shared<Connector>();
 
-    //connector->registerConnection()
+  NodeDefinition defRng1("rng1", "RandomNumberGenerator", {{"seed", 42}});
+  NodeDefinition defRng2("rng2", "RandomNumberGenerator", {{"seed", 69}});
+  NodeDefinition defAdder("adder", "Adder");
+  NodeDefinition defPrinter("printer", "NumberPrinter");
 
+  RandomNumberGenerator rng1(defRng1, connector);
+  RandomNumberGenerator rng2(defRng2, connector);
+  Adder adder(defAdder, connector);
+  NumberPrinter printer(defPrinter, connector);
+
+  connector->connect(Port("rng1", "number"), Port("adder", "a"));
+  connector->connect(Port("rng2", "number"), Port("adder", "b"));
+  connector->connect(Port("adder", "sum"), Port("printer", "number"));
+
+  rng1.execute();
+  rng2.execute();
+  adder.execute();
+  printer.execute();
 }
