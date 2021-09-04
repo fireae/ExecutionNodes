9db767b8a6611a60c60009a6d7391b9d09e0f779
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-04 23:31:30 +0200

    improve logging: add macros and function name

--- include/execution_nodes/logging.hpp
+++ include/execution_nodes/logging.hpp
@@ -22,25 +22,35 @@ class Log {
 public:
   Log();
   virtual ~Log();
-  std::ostringstream &ErrorThrow();
-  std::ostringstream &Error(bool throwException = false);
-  std::ostringstream &Warning();
-  std::ostringstream &Info();
-  std::ostringstream &Debug();
+  std::ostringstream &ErrorThrow(const std::string &functionName = "");
+  std::ostringstream &Error(const std::string &functionName = "",
+                            bool throwException = false);
+  std::ostringstream &Warning(const std::string &functionName = "");
+  std::ostringstream &Info(const std::string &functionName = "");
+  std::ostringstream &Debug(const std::string &functionName = "");
 
 public:
   static LogLevel &getLogLevel();
   static void setLogLevel(LogLevel level);
 
-protected:
+private:
+  std::ostringstream &InternalLog(LogLevel level,
+                                  const std::string &functionName = "",
+                                  bool throwException = false);
+
   bool doThrow;
   std::ostringstream os;
   LogLevel messageLevel;
 
-private:
   Log(const Log &);
   Log &operator=(const Log &) = delete;
   static LogLevel logginglevel;
 };
+
+#define LOG_INFO Log().Info(__func__)
+#define LOG_ERROR Log().Error(__func__)
+#define THROW_ERROR Log().ErrorThrow(__func__)
+#define LOG_DEBUG Log().Debug(__func__)
+#define LOG_WARNING Log().Warning(__func__)
 
 } // namespace execution_nodes
--- src/logging.cpp
+++ src/logging.cpp
@@ -1,71 +1,95 @@
 // log.cpp
 #include <execution_nodes/logging.hpp>
 #include <iostream>
+#include <map>
+#include <chrono>
 
 namespace execution_nodes {
 
 LogLevel Log::logginglevel = LogLevel::LVL_ERROR;
 Log::Log() : doThrow(false) {}
 
-std::string getTimestamp() {
-  auto t = std::time(nullptr);
-  auto tm = *std::localtime(&t);
+
+std::string time_in_HH_MM_SS_MMM() {
+  namespace sc = std::chrono;
+
+  // get current time
+  auto now = sc::system_clock::now();
+
+  // get number of milliseconds for the current second
+  // (remainder after division into seconds)
+  auto ms = sc::duration_cast<sc::milliseconds>(now.time_since_epoch()) % 1000;
+
+  // convert to std::time_t in order to convert to std::tm (broken time)
+  auto timer = sc::system_clock::to_time_t(now);
+
+  // convert to broken time
+  std::tm bt = *std::localtime(&timer);
 
   std::ostringstream oss;
-  oss.clear();
-  oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
-  auto str = oss.str();
-  return str;
+
+  oss << std::put_time(&bt, "%H:%M:%S"); // HH:MM:SS
+  oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
+
+  return oss.str();
 }
+
+std::string getTimestamp() {
+  return time_in_HH_MM_SS_MMM();
+  //auto t = std::time(nullptr);
+  //auto tm = *std::localtime(&t);
+
+  //std::ostringstream oss;
+  //oss.clear();
+  //oss << std::put_time(&tm, "%H:%M:%S");
+  //auto str = oss.str();
+  //return str;
+}
+
+static const std::map<uint8_t, std::string> levelToString = {
+    {static_cast<uint8_t>(LogLevel::LVL_DEBUG), "Debug"},
+    {static_cast<uint8_t>(LogLevel::LVL_ERROR), "Error"},
+    {static_cast<uint8_t>(LogLevel::LVL_INFO), "Info"},
+    {static_cast<uint8_t>(LogLevel::LVL_WARNING), "Warning"},
+};
 
 LogLevel &Log::getLogLevel() { return logginglevel; }
 void Log::setLogLevel(LogLevel level) { logginglevel = level; }
 
-std::ostringstream &Log::ErrorThrow() {
+std::ostringstream &Log::InternalLog(LogLevel level,
+                                     const std::string &functionName,
+                                     bool throwException) {
   os << getTimestamp();
   os << " "
-     << "[Error]"
-     << ": ";
-  messageLevel = LogLevel::LVL_ERROR;
-  doThrow = true;
-  return os;
-}
-
-std::ostringstream &Log::Error(bool throwException) {
-  os << getTimestamp();
-  os << " "
-     << "[Error]"
-     << ": ";
+     << "[" << levelToString.at(static_cast<uint8_t>(level)) << "]";
+  if (functionName != "") {
+    os << " in " << functionName << "():";
+  }
+  os << "\t";
   messageLevel = LogLevel::LVL_ERROR;
   doThrow = throwException;
   return os;
 }
 
-std::ostringstream &Log::Warning() {
-  os << getTimestamp();
-  os << " "
-     << "[Warning]"
-     << ": ";
-  messageLevel = LogLevel::LVL_WARNING;
-  return os;
+std::ostringstream &Log::ErrorThrow(const std::string &functionName) {
+  return InternalLog(LogLevel::LVL_ERROR, functionName, true);
 }
 
-std::ostringstream &Log::Info() {
-  os << getTimestamp();
-  os << " "
-     << "[Info]"
-     << ": ";
-  messageLevel = LogLevel::LVL_INFO;
-  return os;
+std::ostringstream &Log::Error(const std::string &functionName,
+                               bool throwException) {
+  return InternalLog(LogLevel::LVL_ERROR, functionName, throwException);
 }
 
-std::ostringstream &Log::Debug() {
-  os << getTimestamp();
-  os << " "
-     << "[Debug]"
-     << ": ";
-  messageLevel = LogLevel::LVL_DEBUG;
-  return os;
+std::ostringstream &Log::Warning(const std::string &functionName) {
+  return InternalLog(LogLevel::LVL_WARNING, functionName, false);
+}
+
+std::ostringstream &Log::Info(const std::string &functionName) {
+  return InternalLog(LogLevel::LVL_INFO, functionName, false);
+}
+
+std::ostringstream &Log::Debug(const std::string &functionName) {
+  return InternalLog(LogLevel::LVL_DEBUG, functionName, false);
 }
 
 Log::~Log() {
