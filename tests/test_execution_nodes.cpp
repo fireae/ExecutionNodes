f03c73a3017a1e7df83e02cbc9264a2347cead59
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-04 23:35:14 +0200

    add first very basic unit test case

---
+++ tests/test_execution_nodes.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+#include <execution_nodes/execution_nodes.h>
+
+using namespace execution_nodes;
+
+TEST_CASE("Construct Graph", "Test") {
+	Graph graph();
+}
