ff4e473936b2da37bad6f1d9fb1760fbf7894691
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-09 22:29:39 +0200

    Sort include files

--- include/execution_nodes/execution_nodes.h
+++ include/execution_nodes/execution_nodes.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <execution_nodes/connector.h>
-#include <execution_nodes/node.h>
 #include <execution_nodes/graph.h>
 #include <execution_nodes/graph_definition.h>
-
+#include <execution_nodes/node.h>
