dd0ae5791821a43e567473d3d85fe5cf285e9ef6
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-10 22:58:32 +0200

    Remove timestamp from log messages

--- src/logging.cpp
+++ src/logging.cpp
@@ -9,35 +9,6 @@ namespace execution_nodes {
 LogLevel Log::logginglevel = LogLevel::LVL_ERROR;
 Log::Log() : doThrow(false) {}
 
-std::string time_in_HH_MM_SS_MMM() {
-  namespace sc = std::chrono;
-
-  // get current time
-  auto now = sc::system_clock::now();
-
-  // get number of milliseconds for the current second
-  // (remainder after division into seconds)
-  auto ms = sc::duration_cast<sc::milliseconds>(now.time_since_epoch()) % 1000;
-
-  // convert to std::time_t in order to convert to std::tm (broken time)
-  auto timer = sc::system_clock::to_time_t(now);
-
-  // convert to broken time
-  std::tm bt;
-  localtime_s(&bt, &timer);
-
-  std::ostringstream oss;
-
-  oss << std::put_time(&bt, "%H:%M:%S"); // HH:MM:SS
-  oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
-
-  return oss.str();
-}
-
-std::string getTimestamp() {
-  return time_in_HH_MM_SS_MMM();
-}
-
 static const std::map<uint8_t, std::string> levelToString = {
     {static_cast<uint8_t>(LogLevel::LVL_DEBUG), "Debug"},
     {static_cast<uint8_t>(LogLevel::LVL_ERROR), "Error"},
@@ -51,9 +22,7 @@ void Log::setLogLevel(LogLevel level) { logginglevel = level; }
 std::ostringstream &Log::InternalLog(LogLevel level,
                                      const std::string &functionName,
                                      bool throwException) {
-  os << getTimestamp();
-  os << " "
-     << "[" << levelToString.at(static_cast<uint8_t>(level)) << "]";
+  os << "[" << levelToString.at(static_cast<uint8_t>(level)) << "]";
   if (functionName != "") {
     os << " in " << functionName << "():";
   }
