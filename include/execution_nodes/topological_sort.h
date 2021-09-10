1f9f2d1e24d05430fe21c2410b58fe82ec87dd52
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-10 22:22:40 +0200

    Add information to doxygen doc

--- include/execution_nodes/topological_sort.h
+++ include/execution_nodes/topological_sort.h
@@ -8,12 +8,12 @@
 namespace execution_nodes {
 
 /**
- * @brief Applies toplogical sorting of the nodes according to their linear
- * ordering such that for every connection nodeA:portOut->nodeB:portIn
- * from node A to node B, A comes before B in the ordering.
- * This ordering will be the order of execution of the nodes in the graph.
- * Nodes which are not connected to any other node (isolated) will be removed.
- * This function will only succeed if the graph is acyclic.
+ * @brief Applies toplogical sorting of the nodes using depth-first search
+ * according to their linear ordering such that for every connection
+ * nodeA:portOut->nodeB:portIn from node A to node B, A comes before B in the
+ * ordering. This ordering will be the order of execution of the nodes in the
+ * graph. Nodes which are not connected to any other node (isolated) will be
+ * removed. This function will only succeed if the graph is acyclic.
  *
  * @param edges The edges (connections) between the nodes
  * @return std::vector<std::string> The names of the nodes in execution order.
