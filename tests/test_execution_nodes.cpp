c25c8b83a5410a16eedf195b2759796128ae8f1c
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-11 13:23:46 +0200

    Increase test coverage

--- tests/test_execution_nodes.cpp
+++ tests/test_execution_nodes.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <execution_nodes/execution_nodes.h>
+#include <fstream>
 
 using namespace execution_nodes;
 
@@ -48,10 +49,70 @@ public:
   }
 };
 
+class TestNode : public Node {
+public:
+  TestNode(NodeDefinition d, ConnectorPtr c) : Node(d, c) {}
+
+  void execute() {
+    executionTrack.emplace_back(getName());
+    REQUIRE(hasInput("in") == true);
+    REQUIRE(hasInput("zzz") == false);
+    int zzz;
+    bool exceptionOccured = false;
+    try {
+      exceptionOccured = false;
+      getInput("zzz", zzz);
+    } catch (const std::exception &e) {
+      (void)e;
+      exceptionOccured = true;
+    }
+    REQUIRE(exceptionOccured == true);
+
+    auto inNames = getInputPortNames();
+
+    REQUIRE(inNames.size() == 1);
+    REQUIRE(*inNames.begin() == "in");
+
+    auto outNames = getOutputPortNames();
+    REQUIRE(outNames.size() == 1);
+    REQUIRE(*outNames.begin() == "out");
+
+    REQUIRE(getType() == "TestNode");
+
+    try {
+      exceptionOccured = false;
+      setOutput("in", 5);
+    } catch (const std::exception &e) {
+      (void)e;
+      exceptionOccured = true;
+    }
+    REQUIRE(exceptionOccured == true);
+
+    try {
+      exceptionOccured = false;
+      setOutput("foo", 5);
+    } catch (const std::exception &e) {
+      (void)e;
+      exceptionOccured = true;
+    }
+    REQUIRE(exceptionOccured == true);
+
+    try {
+      exceptionOccured = false;
+      setOutput("out", 5);
+    } catch (const std::exception &e) {
+      (void)e;
+      exceptionOccured = true;
+    }
+    REQUIRE(exceptionOccured == false);
+  }
+};
+
 static const NodeRegistry registry = {
     REGISTER(TestSource),
     REGISTER(DummyNode),
     REGISTER(TestSink),
+    REGISTER(TestNode),
 };
 
 std::shared_ptr<Graph> constructABC(int seed) {
@@ -154,5 +215,125 @@ TEST_CASE("Add Connection to Graph", "Test") {
   REQUIRE(executionTrack.size() == 2);
   REQUIRE(executionTrack[0] == "A");
   REQUIRE(executionTrack[1] == "C");
+}
+
+TEST_CASE("Get Output from Node in Graph", "Test") {
+
+  auto graph = constructABC(42);
+  graph->execute();
+  int bout;
+  graph->getOutput(Port("B", "out"), bout);
+  REQUIRE(bout == 42);
+}
+
+TEST_CASE("Fake Output of Node in Graph", "Test") {
+
+  auto graph = constructABC(42);
+  graph->fakeOutput(Port("A", "out"), 69);
+  int aout;
+  graph->getOutput(Port("A", "out"), aout);
+  REQUIRE(aout == 69);
+  graph->execute();
+  graph->getOutput(Port("A", "out"), aout);
+  REQUIRE(aout == 42);
+}
+
+TEST_CASE("Graph from JSON", "Test") {
+
+  executionTrack = {};
+  nlohmann::json j;
+  // json string contains ABC graph definition just like in the other test
+  // cases.
+  j = nlohmann::json::parse(
+      "{\"name\":\"Test ABC "
+      "Graph\",\"nodes\":{\"A\":{\"type\":\"TestSource\",\"settings\":{"
+      "\"seed\":42}},\"B\":{\"type\":\"DummyNode\"},"
+      "\"C\":{\"type\":\"TestSink\",\"settings\":{\"expected\":42}}},"
+      "\"connections\":[[\"A:out\",\"B:in\"],[\"B:out\",\"C:in\"]]}");
+
+  auto gd = loadGraphDefFromJson(j);
+  REQUIRE(gd.name == "Test ABC Graph");
+  REQUIRE(gd.nodes.size() == 3);
+  REQUIRE(gd.nodes[0].name == "A");
+  REQUIRE(gd.nodes[0].type == "TestSource");
+  REQUIRE(gd.nodes[1].name == "B");
+  REQUIRE(gd.nodes[1].type == "DummyNode");
+  REQUIRE(gd.nodes[2].name == "C");
+  REQUIRE(gd.nodes[2].type == "TestSink");
+  REQUIRE(gd.connections.size() == 2);
+
+  auto graph = Graph(gd, registry);
+  REQUIRE(graph.hasNode("A") == true);
+  REQUIRE(graph.hasNode("B") == true);
+  REQUIRE(graph.hasNode("C") == true);
+  graph.execute();
+  REQUIRE(executionTrack.size() == 3);
+  REQUIRE(executionTrack[0] == "A");
+  REQUIRE(executionTrack[1] == "B");
+  REQUIRE(executionTrack[2] == "C");
+}
+
+TEST_CASE("Graph from JSON File", "Test") {
+
+  executionTrack = {};
+
+  std::string content =
+      "{\"name\":\"Test ABC "
+      "Graph\",\"nodes\":{\"A\":{\"type\":\"TestSource\",\"settings\":{"
+      "\"seed\":42}},\"B\":{\"type\":\"DummyNode\"},"
+      "\"C\":{\"type\":\"TestSink\",\"settings\":{\"expected\":42}}},"
+      "\"connections\":[[\"A:out\",\"B:in\"],[\"B:out\",\"C:in\"]]}";
+
+  std::ofstream file("./temp.json");
+  file << content;
+  file.close();
+  auto gd = loadGraphDefFromJsonFile("./temp.json");
+  std::remove("./temp.json");
+  REQUIRE(gd.name == "Test ABC Graph");
+  REQUIRE(gd.nodes.size() == 3);
+  REQUIRE(gd.nodes[0].name == "A");
+  REQUIRE(gd.nodes[0].type == "TestSource");
+  REQUIRE(gd.nodes[1].name == "B");
+  REQUIRE(gd.nodes[1].type == "DummyNode");
+  REQUIRE(gd.nodes[2].name == "C");
+  REQUIRE(gd.nodes[2].type == "TestSink");
+  REQUIRE(gd.connections.size() == 2);
+
+  auto graph = Graph(gd, registry);
+  REQUIRE(graph.hasNode("A") == true);
+  REQUIRE(graph.hasNode("B") == true);
+  REQUIRE(graph.hasNode("C") == true);
+  graph.execute();
+  REQUIRE(executionTrack.size() == 3);
+  REQUIRE(executionTrack[0] == "A");
+  REQUIRE(executionTrack[1] == "B");
+  REQUIRE(executionTrack[2] == "C");
+}
+
+TEST_CASE("Node Checks", "Test") {
+
+  executionTrack = {};
+  nlohmann::json j;
+  // json string contains ABC graph definition just like in the other test
+  // cases.
+  j = nlohmann::json::parse(
+      "{\"name\":\"Test ATC "
+      "Graph\",\"nodes\":{\"A\":{\"type\":\"TestSource\",\"settings\":{"
+      "\"seed\":42}},\"T\":{\"type\":\"TestNode\",\"settings\":null},"
+      "\"C\":{\"type\":\"TestSink\",\"settings\":{\"expected\":5}}},"
+      "\"connections\":[[\"A:out\",\"T:in\"],[\"T:out\",\"C:in\"]]}");
+
+  auto gd = loadGraphDefFromJson(j);
+
+  auto graph = Graph(gd, registry);
+  REQUIRE(graph.hasNode("A") == true);
+  REQUIRE(graph.hasNode("T") == true);
+  REQUIRE(graph.hasNode("C") == true);
+  graph.execute();
+
+  REQUIRE(executionTrack.size() == 3);
+  REQUIRE(executionTrack[0] == "A");
+  REQUIRE(executionTrack[1] == "T");
+  REQUIRE(executionTrack[2] == "C");
 
 }
