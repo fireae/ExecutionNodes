56e2234a9dc977ed59b4bcfad944d8e7bed86f9d
Sebastian Beyer <sebastian.beyer@live.com>
2021-09-11 19:57:13 +0200

    Extend image processing example

--- examples/example_image_processing/main.cpp
+++ examples/example_image_processing/main.cpp
@@ -55,10 +55,11 @@ public:
     CImg<unsigned char> image;
     getInput("image", image);
     CImgDisplay display(image, windowName_.c_str());
-
+    /*
     while (!display.is_closed()) {
       display.wait();
     }
+    */
   }
 
 private:
@@ -78,7 +79,7 @@ public:
     getInput("image", image);
 
     // initialize a binary image version of img
-    CImg<unsigned char> binary(image._width, image._height, 1, 1, 0);
+    CImg<unsigned char> binary(image.width(), image.height(), 1, 1, 0);
 
     cimg_forXY(image, x, y) {
       int gray_value = image(x, y, 0, 0);
@@ -95,10 +96,36 @@ private:
   int threshold_;
 };
 
+class AddImagesNode : public Node {
+
+public:
+  AddImagesNode(const NodeDefinition &definition, const ConnectorPtr &connector)
+      : Node(definition, connector) {}
+
+  void execute() {
+
+    CImg<unsigned char> image1;
+    CImg<unsigned char> image2;
+    getInput("image1", image1);
+    getInput("image2", image2);
+
+    assert(image1.width() == image2.width());
+    assert(image1.height() == image2.height());
+
+    // initialize a binary image version of img
+    CImg<unsigned char> result(image1.width(), image1.height(), 1, 1, 0);
+
+    cimg_forXY(image1, x, y) {
+      result(x, y, 0, 0) = image1(x, y, 0, 0) + image2(x, y, 0, 0);
+    }
+
+    setOutput("result", result);
+  }
+};
+
 static const NodeRegistry registry = {
-    REGISTER(LoadImageNode),
-    REGISTER(GaussianBlurNode),
-    REGISTER(DisplayImageNode),
+    REGISTER(LoadImageNode),     REGISTER(GaussianBlurNode),
+    REGISTER(DisplayImageNode),  REGISTER(AddImagesNode),
     REGISTER(BinarizeImageNode),
 };
 
@@ -106,8 +133,9 @@ int main() {
 
   std::string filePath =
       "C:/Users/Sebastian/source/repos/ExecutionNodes/examples/img_proc.json";
-  auto graphDef = loadGraphDefFromJson(filePath);
+  auto graphDef = loadGraphDefFromJsonFile(filePath);
   Graph graph(graphDef, registry);
-  graph.execute();
 
+  graph.execute(ExecutionMode::SERIAL);
+  graph.execute(ExecutionMode::PARALLEL);
 }
